982e9bbc14c24c744dc4e797249805c4b69e2a29
Lã Kiều Ngọc Quyết <quyetlakn@gmail.com>
2025-07-18 16:39:32 +0700

    Update reference data

--- Reference_Data.sh
+++ Reference_Data.sh
@@ -3,14 +3,20 @@
 ##HG002_Whole_exome_sequencing_Paired-End
 mkdir -p ./HG002 
 fastq-dump SRR2962669 --split-files -O ./HG002
-#Reference_genome
+
+#==================================================================================================
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Reference_genome <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+#==================================================================================================
 ##GRCh38
 mkdir -p ./GRCh38
 wget -P ./GRCh38 https://ftp.ncbi.nlm.nih.gov/ReferenceSamples/giab/release/references/GRCh38/GCA_000001405.15_GRCh38_no_alt_analysis_set.fasta.gz
 ##hg19
 mkdir -p ./hg19
 wget -P ./hg19 https://hgdownload.soe.ucsc.edu/goldenPath/hg19/bigZips/analysisSet/hg19.p13.plusMT.no_alt_analysis_set.fa.gz
-#Benchmark
+
+#===========================================================================================
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Benchmark <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+#===========================================================================================
 mkdir -p ./Benchmark
 wget -P ./Benchmark https://ftp.ncbi.nlm.nih.gov/ReferenceSamples/giab/release/AshkenazimTrio/HG002_NA24385_son/NISTv4.2.1/GRCh38/HG002_GRCh38_1_22_v4.2.1_benchmark.vcf.gz
 wget -P ./Benchmark https://ftp.ncbi.nlm.nih.gov/ReferenceSamples/giab/release/AshkenazimTrio/HG002_NA24385_son/NISTv4.2.1/GRCh38/HG002_GRCh38_1_22_v4.2.1_benchmark.vcf.gz.tbi
