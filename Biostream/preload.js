a1b0f350e18f81632fb8d1f7478b37d51e66631c
Lã Kiều Ngọc Quyết <quyetlakn@gmail.com>
2025-08-01 22:23:31 +0700

    Update preload.js

--- Biostream/preload.js
+++ Biostream/preload.js
@@ -1,11 +1,12 @@
-const { contextBridge, ipcRenderer} = require('electron');
+const { contextBridge, ipcRenderer } = require('electron');
 const si = require('systeminformation');
 const os = require('os');
+const fs = require('fs');
 
 contextBridge.exposeInMainWorld('electronAPI', {
-	getSystemMemoryInfo: () => process.getSystemMemoryInfo(),
-	getCPUUsage: () => process.getCPUUsage(),
-	getDiskInfo: async () => {
+  getSystemMemoryInfo: () => process.getSystemMemoryInfo(),
+  getCPUUsage: () => process.getCPUUsage(),
+  getDiskInfo: async () => {
     try {
       const data = await si.fsSize();
       return data.map(disk => ({
@@ -17,5 +18,10 @@ contextBridge.exposeInMainWorld('electronAPI', {
       console.error('Disk info error:', err);
       return [];
     }
-  }
+  },
+
+  // ==== THÊM CÁC CHỨC NĂNG CỬA SỔ ====
+  minimizeWindow: () => ipcRenderer.send('minimize-window'),
+  maximizeWindow: () => ipcRenderer.send('maximize-window'),
+  closeWindow: () => ipcRenderer.send('close-window')
 });
